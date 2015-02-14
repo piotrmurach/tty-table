93d23e33abcee806bf9f1ea675b7dba7d5ee6b61
Piotr Murach <pmurach@gmail.com>
2015-02-14 15:36:13 +0000

    Change data specs.

--- spec/unit/data_spec.rb
+++ spec/unit/data_spec.rb
@@ -2,13 +2,11 @@
 
 require 'spec_helper'
 
-RSpec.describe TTY::Table, '#data' do
-  let(:header) { ['h1', 'h2', 'h3'] }
-  let(:rows)   { [['a1', 'a2', 'a3'], ['b1', 'b2', 'b3']] }
-
-  subject(:table) { described_class.new header, rows }
-
+RSpec.describe TTY::Table, '.data' do
   it 'gets all table data' do
+    header = ['h1', 'h2', 'h3']
+    rows = [['a1', 'a2', 'a3'], ['b1', 'b2', 'b3']]
+    table = described_class.new header, rows
     expect(table.data).to eql([header] + rows)
   end
 end
