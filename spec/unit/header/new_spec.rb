f8ad15d251e6f5cef601a28acfe7717c0327f337
Piotr Murach <pmurach@gmail.com>
2015-02-15 11:11:51 +0000

    Clean up header spec.

--- spec/unit/header/new_spec.rb
+++ spec/unit/header/new_spec.rb
@@ -3,23 +3,14 @@
 require 'spec_helper'
 
 RSpec.describe TTY::Table::Header, '#new' do
-  let(:object) { described_class }
 
-  context 'with no arguments' do
-    subject { object.new }
-
-    it { is_expected.to be_instance_of(object) }
-
-    it { is_expected.to be_empty }
+  it "is empty without arguments" do
+    header = TTY::Table::Header.new
+    expect(header).to be_empty
   end
 
-  context 'with attributes' do
-    subject { object.new(attributes) }
-
-    let(:attributes) { ['id', 'name', 'age'] }
-
-    it { is_expected.to be_instance_of(object) }
-
-    it { is_expected == attributes }
+  it "isn't empty with attributes" do
+    header = TTY::Table::Header.new [:id, :name, :age]
+    expect(header.to_a).to eq([:id, :name, :age])
   end
 end
