7dad8ec7727dd1e6fd7dc7769ad2fb1501a3e5f0
Piotr Murach <pmurach@gmail.com>
2015-02-28 14:19:22 +0000

    Add specs for multiline column widths.

--- spec/unit/column_set/extract_widths_spec.rb
+++ spec/unit/column_set/extract_widths_spec.rb
@@ -11,11 +11,27 @@ RSpec.describe TTY::Table::ColumnSet, '#extract_widths' do
     expect(column_set.extract_widths).to eql([2,2,2])
   end
 
-  it "extracts widhts from utf" do
+  it "extracts widths from utf" do
     header = ['h1', 'うなじ']
     rows   = [['こんにちは', 'a2'], ['b1','選択']]
     table  = TTY::Table.new header, rows
     column_set = TTY::Table::ColumnSet.new(table)
     expect(column_set.extract_widths).to eql([10,6])
+  end
+
+  it "extracts widths from multiline text" do
+    table = TTY::Table.new
+    table << ["Multi\nLine\nContent", "Text\nthat\nwraps"]
+    table << ["Some\nother\ntext", 'Simple']
+    column_set = TTY::Table::ColumnSet.new(table)
+    expect(column_set.extract_widths).to eq([7,6])
+  end
+
+  it "extracts widths from multiline text" do
+    table = TTY::Table.new
+    table << ["Multi\\nLine\\nContent", "Text\\nthat\\nwraps"]
+    table << ["Some\\nother\\ntext", 'Simple']
+    column_set = TTY::Table::ColumnSet.new(table)
+    expect(column_set.extract_widths).to eq([20, 17])
   end
 end
