bb32bc8fd733a46f6be65c1ce66fc1febe085542
Piotr Murach <pmurach@gmail.com>
2018-01-12 23:32:00 +0100

    Change to use relative path

--- benchmarks/speed.rb
+++ benchmarks/speed.rb
@@ -1,17 +1,15 @@
 # coding: utf-8
 
-# Benchmark speed of table operations
-
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-
-require 'tty-table'
 require 'benchmark'
 require 'benchmark/ips'
+
+require_relative '../lib/tty-table'
 
 header = [:name, :color]
 rows   = (1..100).map { |n| ["row#{n}", "red"] }
 table  = TTY::Table.new(header, rows)
 
+# Benchmark speed of table operations
 Benchmark.ips do |r|
   r.report("Ruby #to_s") do
     rows.to_s
