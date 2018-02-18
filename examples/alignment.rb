d15915b78670a7cc0a54364a89758891e180e6af
Piotr Murach <pmurach@gmail.com>
2018-02-18 17:25:00 +0000

    Change examples to use local gem version

--- examples/alignment.rb
+++ examples/alignment.rb
@@ -1,8 +1,8 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
-require 'tty-table'
+require_relative '../lib/tty-table'
 
-table = TTY::Table.new header: ['header1', 'header2', 'header3']
+table = TTY::Table.new header: ['right align', 'center align', 'left align']
 table << ['a1', 'a2', 'a3']
 table << ['b1','b2', 'b3']
 table << ['c1', 'c2', 'c3']
--- examples/basic.rb
+++ examples/basic.rb
@@ -1,6 +1,6 @@
-# encoding: utf-8
+# frozen_string_literal: true
 
-require 'tty-table'
+require_relative '../lib/tty-table'
 
 table = TTY::Table.new ['header1','header2'], [['a1', 'a2'], ['b1', 'b2']]
 puts table.render(:basic)
