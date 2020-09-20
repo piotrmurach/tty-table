680919b7a7060220973959af3e5da2d63b30bd50
Piotr Murach <pmurach@gmail.com>
2020-09-20 16:05:11 +0100

    Change null border to specify all parts and remove top and bottom line overrides

--- lib/tty/table/border/null.rb
+++ lib/tty/table/border/null.rb
@@ -7,16 +7,22 @@ module TTY
     class Border
       # A class that represents no border.
       class Null < Border
-
         def_border do
-          center SPACE_CHAR
-        end
-
-        # A stub top line
-        #
-        # @api private
-        def top_line
-          border_options ? super : nil
+          top           EMPTY_CHAR
+          top_mid       EMPTY_CHAR
+          top_left      EMPTY_CHAR
+          top_right     EMPTY_CHAR
+          bottom        EMPTY_CHAR
+          bottom_mid    EMPTY_CHAR
+          bottom_left   EMPTY_CHAR
+          bottom_right  EMPTY_CHAR
+          mid           EMPTY_CHAR
+          mid_mid       EMPTY_CHAR
+          mid_left      EMPTY_CHAR
+          mid_right     EMPTY_CHAR
+          left          EMPTY_CHAR
+          center        SPACE_CHAR
+          right         EMPTY_CHAR
         end
 
         # A stub separator line
@@ -25,13 +31,6 @@ module TTY
         def separator
           return [] if border_options.separator # == EACH_ROW #TODO ask about this looks broken.
           # how could border_options ever be nil, if we just did border_options.separator
-          border_options ? super : nil
-        end
-
-        # A stub bottom line
-        #
-        # @api private
-        def bottom_line
           border_options ? super : nil
         end
       end # Null
