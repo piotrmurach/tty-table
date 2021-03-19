9f694a5203662fe3735dd23bd397c13e91bc036a
Paulo Gorgita <paulo.gorgita@swisscom.com>
2021-03-19 09:26:56 +0100

    specs updated with more columns to replicate piotrmurach/issues/35

--- spec/unit/rotate_spec.rb
+++ spec/unit/rotate_spec.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 RSpec.describe TTY::Table, '#rotate' do
-  let(:header) { ['h1', 'h2', 'h3'] }
-  let(:rows) { [['a1', 'a2', 'a3'], ['b1', 'b2', 'b3']] }
+  let(:header) { ['h1', 'h2', 'h3', 'h4', 'h5'] }
+  let(:rows) { [['a1', 'a2', 'a3', 'a4', 'a5'], ['b1', 'b2', 'b3', 'b4', 'b5']] }
 
   subject(:table) { described_class.new(header, rows) }
 
@@ -34,9 +34,13 @@ RSpec.describe TTY::Table, '#rotate' do
         ['1', 'a1'],
         ['2', 'a2'],
         ['3', 'a3'],
+        ['4', 'a4'],
+        ['5', 'a5'],
         ['1', 'b1'],
         ['2', 'b2'],
         ['3', 'b3'],
+        ['4', 'b4'],
+        ['5', 'b5']
       ]
       expect(table.header).to be_nil
     end
@@ -49,11 +53,11 @@ RSpec.describe TTY::Table, '#rotate' do
       expect(table.header).to eql header
     end
 
-    it 'roates the output' do
-      expect(table.to_s).to eq("a1 a2 a3\nb1 b2 b3")
+    it 'rotates the output' do
+      expect(table.to_s).to eq("a1 a2 a3 a4 a5\nb1 b2 b3 b4 b5")
       table.orientation = :vertical
       table.rotate
-      expect(table.to_s).to eq("1 a1\n2 a2\n3 a3\n1 b1\n2 b2\n3 b3")
+      expect(table.to_s).to eq("1 a1\n2 a2\n3 a3\n4 a4\n5 a5\n1 b1\n2 b2\n3 b3\n4 b4\n5 b5")
     end
   end
 
@@ -64,9 +68,13 @@ RSpec.describe TTY::Table, '#rotate' do
         ['h1', 'a1'],
         ['h2', 'a2'],
         ['h3', 'a3'],
+        ['h4', 'a4'],
+        ['h5', 'a5'],
         ['h1', 'b1'],
         ['h2', 'b2'],
         ['h3', 'b3'],
+        ['h4', 'b4'],
+        ['h5', 'b5']
       ]
       expect(table.header).to be_empty
     end
